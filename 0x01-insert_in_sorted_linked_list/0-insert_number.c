85f450a5878fced5b05d16c145c614bc73616905
UsmanGTA <usmangta@gmail.com>
2020-12-17 18:52:25 -0800

    added

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -10,15 +10,24 @@ listint_t *insert_node(listint_t **head, int insert)
 	listint_t *new = NULL, *cur = (*head), *next = NULL;
 
 	/* NULL CHECK */
-	if (!head)
+	if (!head || !(*head))
 		return (NULL);
 
 	new = malloc(sizeof(listint_t));
 	new->n = insert;
 
+	if (!cur->next || insert < 0)
+	{
+		if (cur->n >= insert)
+			new->next = cur, (*head) = new;
+		else
+			cur->next = new;
+		return (new);
+	}
+
 	/* Finally, a decent linked list */
 	for (next = cur->next; next; cur = cur->next, next = next->next)
-		if (cur->n >= insert)
+		if (cur->n <= insert && next->n >= insert)
 			break;
 
 	cur->next = new, new->next = next;
