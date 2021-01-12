4f32716c34f08ff9ea24eb2003107458748516be
UsmanGTA <usmangta@gmail.com>
2021-01-12 20:49:02 +0500

    added improved code

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,30 +1,6 @@
 #include "sandpiles.h"
 #include <stdio.h>
 
-static void print_grid_sum(int grid1[3][3], int grid2[3][3])
-{
-    int i, j;
-
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid1[i][j]);
-        }
-
-        printf(" %c ", (i == 1 ? '+' : ' '));
-
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid2[i][j]);
-        }
-        printf("\n");
-    }
-}
 static void print_grid(int grid[3][3])
 {
     int i, j;
@@ -41,8 +17,6 @@ static void print_grid(int grid[3][3])
     }
 }
 
-
-
 /**
  * sandpiles_sum - returns a stabilized sandpile
  * @grid1: first 3x3 grid
@@ -56,9 +30,8 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
     /* Add up the two grids into grid1 */
     for (columns = 0; columns < 3; columns++)
         for (rows = 0; rows < 3; rows++)
-            grid1[columns][rows] = grid1[columns][rows] + grid2[columns][rows];
+            grid1[columns][rows] += + grid2[columns][rows];
 
-	print_grid_sum(grid1, grid2);
     stabilized(grid1);
 }
 
@@ -66,6 +39,8 @@ void stabilized(int grid[3][3])
 {
 	int rows, columns, unbalanced = 0;
 
+	printf("=\n");
+	print_grid(grid);
 	for (columns = 0; columns < 3; columns++)
 		for (rows = 0; rows < 3; rows++)
 			if (grid[columns][rows] > 3)
@@ -84,8 +59,6 @@ void stabilized(int grid[3][3])
 
 				unbalanced = 1; /* True, yes it was unbalanced */
             }
-
-	print_grid(grid);
     if (unbalanced == 1)
 		stabilized(grid);
 }
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,6 +1,8 @@
 #ifndef _SANDPILES_H
 #define _SANDPILES_H
 
+#include <stdio.h>
+
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 void stabilized(int summed_up_grid[3][3]);
 
