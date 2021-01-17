f3077cb4a8e0d5f2f9a1f680bc1e4b0366ff75e2
UsmanGTA <hi@usmanjabbar.com>
2021-01-17 23:51:13 +0000

    Perfected code + Tested Edge cases

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -2,19 +2,18 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *s = *head, *d = *head;
-	int i = 0, *half_arr = malloc(sizeof(int));
-	
+	listint_t *s = *head, *d = *head;			/* s = single leap | d = double leap */
+	int i = 0, *half_arr = malloc(sizeof(int));	/* Use this to dyanmically add items to array */
+
+	/* NULL CHECKS */
+	if (!head || !(*head) || !half_arr)
+		return(0);
+
 	/* This loop searches for the mid */
 	/* By the end of the loop, s should */
 	/* be pointing to the middle list */
 	while (d->next && d->next->next)
 	{
-		/* s = s->next;		 Single leaps */
-		/* d = d->next->next;  Double leaps */
-
-		printf("'Boutta add %d to half_array[%d]\n", s->n, i);
-
 		half_arr[i] = s->n;						/* Add each element s goes through */
 		i++;									/* Increment so that realloc could use the i */
 		half_arr = realloc(half_arr, i + 1);	/* Add an extra block of memory for the next loop */
@@ -22,14 +21,22 @@ int is_palindrome(listint_t **head)
 		s = s->next;
 		d = d->next->next;
 	}
+	half_arr[i] = s->n;							/* Add the last n in s */
+
+	/* Since i could tell us whether the len of the list is even/odd */
+	/* we could take advantage of that and skip the current s if len(ll) */
+	/* is an odd */
+
+	/* NOTE: Since i started at 0, the odd/even check looks inversed */
+	if (i % 2 == 0)
+		s = s->next;
 
 	/* Now that we have the len of s stored in i */
 	/* we could decrement i while simulataneously */
 	/* move s forward all the way to the end */
 	while (s)
 	{
-		printf("Boutta compare %d with %d\n", s->n, half_arr[i - 1]);
-		if (s->n != half_arr[i - 1])
+		if (s->n != half_arr[i])
 			return(0);
 
 		s = s->next;
