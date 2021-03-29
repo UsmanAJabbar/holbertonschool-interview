5532c3100e7b566e1071bfae1fe389af8a668b51
UsmanGTA <hi@usmanjabbar.com>
2021-03-29 12:01:56 +0500

    Added initial commit for task 0

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,19 @@
+#include "search.h"
+
+
+/**
+ * linear_skip - searches for a specific
+ * value in an express-powered linked list
+ * @head: address to the head of the list
+ * @value: value to look for in the list
+ * Return: Returns address to the node | NULL
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	if (!list || list->n == value || list->n > value)
+		return (list);
+
+	if (!list->express || (list->n < value) < list->express->n)
+		return linear_skip(list->next, value);
+	return linear_skip(list->express, value);
+}
