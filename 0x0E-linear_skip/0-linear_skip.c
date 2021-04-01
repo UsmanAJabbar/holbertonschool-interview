94064b7b7efd72e086784521b529629f7c280c78
UsmanGTA <hi@usmanjabbar.com>
2021-04-01 13:25:04 +0500

    Added initial commit

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -8,12 +8,31 @@
  * @value: value to look for in the list
  * Return: Returns address to the node | NULL
  */
-skiplist_t *linear_skip(skiplist_t *list, int value)
+skiplist_t *linear_skip(skiplist_t *head, int value) {
+	int flag = 0;
+
+	if (!head)
+		return (NULL);
+
+	return linear_skip_helper(head, value, head->index, head->n, flag);
+}
+
+skiplist_t *linear_skip_helper(skiplist_t *list, int target, size_t index, int value, int flag)
 {
-	if (!list || list->n == value || list->n > value)
+	if (!list || list->n > target)
+		return (NULL);
+	if (list->n == target)
 		return (list);
 
-	if (!list->express || (list->n < value) < list->express->n)
-		return linear_skip(list->next, value);
-	return linear_skip(list->express, value);
+	printf("Value checked at index [%zu] = [%d]\n", index, value);
+
+	if (list->express && target >= list->express->n)
+		return linear_skip_helper(list->express, target, list->express->index, list->express->n, flag);
+
+	if (!flag && list->express)
+	{
+		printf("Value found between indexes [%zu] and [%zu]\n", list->index, list->express->index);
+		flag = 1;
+	}
+	return linear_skip_helper(list->next, target, list->next->index, list->next->n, flag);
 }
--- 0x0E-linear_skip/search.h
+++ 0x0E-linear_skip/search.h
@@ -26,10 +26,12 @@ typedef struct skiplist_s
     struct skiplist_s *express;
 } skiplist_t;
 
+
+skiplist_t *linear_skip(skiplist_t *head, int value);
+skiplist_t *linear_skip_helper(skiplist_t *list, int target, size_t index, int value, int flag);
+
 skiplist_t *create_skiplist(int *array, size_t size);
 void print_skiplist(const skiplist_t *list);
 void free_skiplist(skiplist_t *list);
-
-skiplist_t *linear_skip(skiplist_t *head, int value);
 
 #endif /* _SEARCH_H_ */
