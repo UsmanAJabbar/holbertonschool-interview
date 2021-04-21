e1f7bbd1bed979a5abe63fa8ff6c91a84a4ce8f1
UsmanGTA <hi@usmanjabbar.com>
2021-04-21 06:45:54 +0500

    Added initial semistandard fixes

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,15 +3,15 @@
 const request = require('request');
 const filmNo = process.argv[2];
 
-const character_print = (character_api_urls, index) => {
-  if (character_api_urls[index]) {
-    request.get(character_api_urls[index], (error, response, body) => {
-      const character_name = JSON.parse(body).name;
-      console.log(character_name);
-      character_print(character_api_urls, index + 1);
-    })
+const character_print = (characterAPIurls, index) => {
+  if (characterAPIurls[index]) {
+    request.get(characterAPIurls[index], (error, response, body) => {
+      const characterName = JSON.parse(body).name;
+      console.log(characterName);
+      character_print(characterAPIurls, index + 1);
+    });
   }
-}
+};
 
 request.get(`https://swapi-api.hbtn.io/api/films/${filmNo}`, (error, response, body) => {
   if (!error && response.statusCode === 200) {
