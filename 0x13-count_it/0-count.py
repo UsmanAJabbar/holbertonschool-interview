c0ee217df2174121df455985b60487e42c715ec9
UsmanGTA <usmangta@gmail.com>
2021-05-07 09:58:42 +0500

    Added fixes

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -6,20 +6,17 @@ from collections import Counter
 
 def count_words(sub, word_list=[], next='', word_sum={}) -> None:
     """Counts the number of words"""
-    print(word_sum)
-
     # Converts it to a set on the very first run
-    if not word_sum:   word_list = set([word.lower() for word in word_list])
-
-    # This executes when there are no more pages
-    if next == 'None': return print(word_sum)
+    if not word_sum:
+        word_list = set([word.lower() for word in word_list])
 
     api = r_get(f'https://api.reddit.com/r/{sub}/hot?after={next}',
                 headers={'User-Agent': 'Chrome'},
                 allow_redirects=False).json()
     posts = api.get('data', {}).get('children')
 
-    if not posts: return print(word_sum)
+    if not posts:
+        return print_helper(word_sum)
 
     for post in posts:
         if not post['data']['stickied']:
@@ -32,9 +29,14 @@ def count_words(sub, word_list=[], next='', word_sum={}) -> None:
                 else:
                     word_sum[keyword]  = w_count.get(keyword, 0)
 
-    next = api.get('data', {}).get('after', 'None')
-    count_words(sub, word_list, next, word_sum)
+    next = api.get('data', {}).get('after', 'exit')
+    # This executes when there are no more pages or if data doesn't exist
+    if next in ['exit', None]:
+        return print_helper(word_sum)
 
-# def print_helper(word_count: dict) -> None:
-#     """Results should be printed in descending order, by the count"""
-#     print(word_count)
+    return count_words(sub, word_list, next, word_sum)
+
+def print_helper(word_count: dict) -> None:
+    """Results should be printed in descending order, by the count"""
+    for elements in sorted(word_count.items(), key=lambda x: x[1]):
+        print(f'{elements[0]}: {elements[1]}')
