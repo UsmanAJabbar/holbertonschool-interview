1e986f844faaa00d4a413c0209cf8afa4d0d1df6
UsmanGTA <hi@usmanjabbar.com>
2021-06-12 03:33:34 +0000

    Simplifed code

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,10 +6,6 @@ def rotate_2d_matrix(matrix):
     """Rotate 2D Matrix"""
     size = len(matrix)
 
-    new_arr = []
     for column in range(size):
-        new_arr += [[matrix[row][column]
-                     for row in range(size - 1, -1, -1)]]
-
-    for row in range(size):
-        matrix[row] = new_arr[row]
+        matrix[column] = [matrix[row][column]
+                          for row in range(size - 1, -1, -1)]
