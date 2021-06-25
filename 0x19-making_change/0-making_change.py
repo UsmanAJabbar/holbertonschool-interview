9266526d6a2aa1e15bd4016af7835ee4e184291c
UsmanGTA <hi@usmanjabbar.com>
2021-06-25 03:52:36 +0000

    Added

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -2,30 +2,45 @@
 """Some doc"""
 
 
-def makeChange(coins: list, total: int) -> int:
+def makeChange(coins: list, amount: int) -> int:
     """
     ------------------
     METHOD: makeChange
     ------------------
     Given a pile of coins of different values,
     determine the fewest number of coins needed
-    to meet a given total total.
+    to meet a given amount.
     Args:
         @coins: list of coin options
-        @total: total to generate
+        @amount: amount to generate
     """
-    if total == 0:
-        return 0
+    inf = float('inf')
+
+    # Create a table the size of `amount`
+    cache = [inf] * (amount + 1)
+    cache[0] = 0
+
+    # Plan, check out how many coins it takes to make the current coin
+    # So we're going to loop from 0 to the amount and record how many coins
+    # it takes to get to that amount
+
+    # How many coins does it take to make 0? 1
+    # How many coins does it take to make 1? 1  # Given that we have the coin
+    # and so on and so forth, this is a linear approach so the cache list doesn't recreate itself
+
+    # By the end of it, since we recorded how many coins it took at each index
+    # we could simply return how many coins it took to make `amount`
+
+    # cache[index] => where cache is the table and [index] represents
+                                                    # the index
+                                                    # and the amount
+
+    for current_amount in range(amount + 1): # Loop from 0 to amount
+        for coin in coins:                   # Find coins that could feasibly be generate the current_amount
+            if coin <= current_amount:       # We can only generate an amount if the coin is smaller than the amount
+                cache[current_amount] = min(cache[current_amount], cache[current_amount - coin])
+                                             # Compare the current_amount with the amount if we have previously seen it
+                                             # eg, current_amount = 25, coin = 5
+                                             # check if the cache[25] smaller number of coins or cache[20]
     
-# There is no way  the result ,i.e. the no. of coins to form the total, can be greater than the actual given total so we initiate our dp table with 'total+1' to compare minimums
-
-    dp = [total+1] * (total+1) 
-    dp[0] = 0
-    
-# we are not interseted in the 'total' less than current coin's value as there is no possible way to include this coin and produce an total less than it.
-
-    for i in range(len(coins)):
-        for j in range(coins[i], total + 1):  
-            dp[j] = min(dp[j], dp[j - coins[i]] + 1)
-
-    return -1 if dp[-1] > total else dp[-1]
+    return cache[amount] if cache[amount] != inf else -1
