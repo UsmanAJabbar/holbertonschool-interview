98de88eff5deb4f3d5500236042f9358065b664e
UsmanGTA <hi@usmanjabbar.com>
2021-06-25 03:31:12 +0000

    Testing timeout

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -9,19 +9,23 @@ def makeChange(coins: list, total: int) -> int:
     ------------------
     Given a pile of coins of different values,
     determine the fewest number of coins needed
-    to meet a given amount total.
+    to meet a given total total.
     Args:
         @coins: list of coin options
         @total: total to generate
     """
-    if amount == 0:
+    if total == 0:
         return 0
+    
+# There is no way  the result ,i.e. the no. of coins to form the total, can be greater than the actual given total so we initiate our dp table with 'total+1' to compare minimums
 
-    dp = [amount+1] * (amount+1) 
+    dp = [total+1] * (total+1) 
     dp[0] = 0
+    
+# we are not interseted in the 'total' less than current coin's value as there is no possible way to include this coin and produce an total less than it.
 
     for i in range(len(coins)):
-        for j in range(coins[i], amount + 1):  
+        for j in range(coins[i], total + 1):  
             dp[j] = min(dp[j], dp[j - coins[i]] + 1)
 
-    return -1 if dp[-1] > amount else dp[-1]
+    return -1 if dp[-1] > total else dp[-1]
