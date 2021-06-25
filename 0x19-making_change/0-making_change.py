7ca04f215df3238078d62e79694a9dd1bae486b0
UsmanGTA <hi@usmanjabbar.com>
2021-06-25 04:03:17 +0000

    Added PEP8 fixes

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -29,7 +29,7 @@ def makeChange(coins: list, amount: int) -> int:
 
     # How many coins does it take to make 0? 1
     # How many coins does it take to make 1? 1  # Given that we have the coin
-    # and so on and so forth, this is a linear approach so the cache list doesn't recreate itself
+    # and so on and so forth
 
     # By the end of it, since we recorded how many coins it took at each index
     # we could simply return how many coins it took to make `amount`
@@ -39,11 +39,11 @@ def makeChange(coins: list, amount: int) -> int:
     #                                                           and the amount
 
     for current_amount in range(amount + 1):  # Loop from 0 to amount
-        for coin in coins:                   # Find coins that could feasibly be generate the current_amount
-            if coin <= current_amount:       # We can only generate an amount if the coin is smaller than the amount
+        for coin in coins:                    # coins that could gen the amt
+            if coin <= current_amount:        # gen amt if coin is <= amount
                 cache[current_amount] = min(
                     cache[current_amount], cache[current_amount - coin] + 1)
-                # Compare the current_amount with the amount if we have previously seen it
+                # Compare cur_amt with the amount if we have previously seen it
                 # eg, current_amount = 25, coin = 5
                 # check if the cache[25] smaller number of coins or cache[20]
 
