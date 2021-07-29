d570d073ce3877ea6e9ba77ab424fd88a094be69
UsmanGTA <hi@usmanjabbar.com>
2021-07-29 21:38:21 +0000

    Added

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -8,27 +8,19 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (!s1 || !s2)
-		return (1);
+	/* Check if S1 or S2 is over */
+	if (*s1 == '\0' || *s2 == '\0')
+	{
+		if (*s1 == '\0' && *s2 == '*' && *(s2 + 1) == '\0')
+			return (1);
+		return (*s1 == '\0' && *s2 == '\0');
+	}
 
-	/*
-	# MASTER PLAN
-	# Go through the whole string
-	# If *s1 and *s2 are on a wildcard, increment both of them
-	# If only s2 has a wildcard
-	#	if s2[i + 1] == NULL return true (aka 1)
-	#	else:
-	#		increment s1 until s2[i + 1] aren't the same
-	#		then increment both as usual with an strcmp
-	*/
-	if (*s1 == '*' && *s2 == '*')
-		return wildcmp(s1 + 1, s2 + 1);
-	else if (*s2 == '*')
-		return wildcmp(s1 + 1, s2);
-	else if (*s1 == '*')
-		return wildcmp(s1, s2 + 1);
-	else if (*s1 == *s2)
-		return wildcmp(s1 + 1, s2 + 1);
-	else
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+
+	if (*s1 != *s2)
 		return (0);
+
+	return (wildcmp(s1 + 1, s2 + 1));
 }
--- 0x1E-wild_cmp/holberton.h
+++ 0x1E-wild_cmp/holberton.h
@@ -1,6 +1,8 @@
 #ifndef _HOLBERTON_H
 #define _HOLBERTON_H
 
+#include <stdio.h>
+
 int wildcmp(char *s1, char *s2);
 
 #endif
