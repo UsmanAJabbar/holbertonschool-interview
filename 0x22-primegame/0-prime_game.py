72ffd0c4335e523063bbdcd7df0efe235852ba63
UsmanGTA <hi@usmanjabbar.com>
2021-08-24 00:23:52 +0500

    Added

---
+++ 0x22-primegame/0-prime_game.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Prime Numbers"""
+
+cache = {}
+
+def set_generator(number):
+    """Generates a set from {1...num}"""
+    return {
+        num for num in range(1, number + 1)
+    }
+
+def play_game(playset, playset_max_num):
+    """Plays a game eliminating all of the prime numbers"""
+    # Find the smallest prime number and assumes the initial user chooses the prime number
+    playcount = 0
+    for num in playset:
+        if num != 1 and is_prime(num):
+            remove_multiples(num, playset, playset_max_num)
+            playcount += 1
+    
+    return (playcount % 2 == 0)
+
+
+def is_prime(number):
+    """Returns whether a given number is a prime"""
+    if number in cache:
+        return cache[number]
+
+    for num in range(1, number):
+        if number % num == 0:
+            cache[number] = False
+            return cache[number]
+
+    cache[number] = True
+    return cache[number]
+
+def remove_multiples(prime_number, number_set, number_set_max_num):
+    """Removes all of the multiples of num from number_set"""
+    i = 1
+    while prime_number * i <= number_set_max_num:
+        number_set.remove(prime_number * i)
+        i += 1
+
+def isWinner(x, nums):
+    """Some game"""
+
+    win_count = {
+        'True': 0,
+        'False': 0
+    }
+
+    for num in nums:
+        playset = set_generator(num)
+        win = play_game(playset, num)
+        win_count[str(win)] += 1
+    
+    return (
+        'Ben'
+            if win_count['True'] > win_count['False']
+            else
+        'Maria'
+    )
