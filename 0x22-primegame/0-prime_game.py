0ca6fdc27a27cb8d7cee1156f6bc1ec1b4c6922d
UsmanGTA <hi@usmanjabbar.com>
2021-08-26 02:04:10 +0500

    Added

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -3,11 +3,13 @@
 
 cache = {}
 
+
 def set_generator(number):
     """Generates a set from {1...num}"""
     return {
-        num for num in range(1, number + 1)\
+        num for num in range(1, number + 1)
     }
+
 
 def play_game(playset, playset_max_num):
     """Plays a game eliminating all of the prime numbers"""
@@ -15,7 +17,6 @@ def play_game(playset, playset_max_num):
     playcount = 0
     for num in playset:
         if num != 1 and is_prime(num):
-            # remove_multiples(num, playset, playset_max_num)
             playcount += 1
 
     return (playcount % 2 == 0)
@@ -34,12 +35,14 @@ def is_prime(number):
     cache[number] = True
     return cache[number]
 
+
 def remove_multiples(prime_number, number_set, number_set_max_num):
     """Removes all of the multiples of num from number_set"""
     i = 1
     while prime_number * i <= number_set_max_num:
         number_set.remove(prime_number * i)
         i += 1
+
 
 def isWinner(x, nums):
     """Some game"""
@@ -61,7 +64,7 @@ def isWinner(x, nums):
 
     return (
         'Ben'
-            if win_count['True'] > win_count['False']
-            else
+        if win_count['True'] > win_count['False']
+        else
         'Maria'
     )
