a8152ea65dbe28ace81fce7060d2ebebd0451da5
UsmanGTA <hi@usmanjabbar.com>
2021-08-26 02:00:24 +0500

    Added

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -6,7 +6,7 @@ cache = {}
 def set_generator(number):
     """Generates a set from {1...num}"""
     return {
-        num for num in range(1, number + 1)
+        num for num in range(1, number + 1)\
     }
 
 def play_game(playset, playset_max_num):
@@ -15,9 +15,9 @@ def play_game(playset, playset_max_num):
     playcount = 0
     for num in playset:
         if num != 1 and is_prime(num):
-            remove_multiples(num, playset, playset_max_num)
+            # remove_multiples(num, playset, playset_max_num)
             playcount += 1
-    
+
     return (playcount % 2 == 0)
 
 
@@ -26,7 +26,7 @@ def is_prime(number):
     if number in cache:
         return cache[number]
 
-    for num in range(1, number):
+    for num in range(2, number):
         if number % num == 0:
             cache[number] = False
             return cache[number]
@@ -49,11 +49,16 @@ def isWinner(x, nums):
         'False': 0
     }
 
+    if (x <= 0 or len(nums) <= 0):
+        return None
+
     for num in nums:
         playset = set_generator(num)
         win = play_game(playset, num)
         win_count[str(win)] += 1
-    
+
+    # print(win_count)
+
     return (
         'Ben'
             if win_count['True'] > win_count['False']
