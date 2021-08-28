878a9d61eae5b472d2398457291adc74b6ae1d11
UsmanGTA <hi@usmanjabbar.com>
2021-08-28 17:26:55 +0500

    Added PEP8 fixes

--- 0x22-primegame/0-main.py
+++ 0x22-primegame/0-main.py
@@ -3,6 +3,7 @@
 Main file for testing
 """
 
+
 isWinner = __import__('0-prime_game').isWinner
 
 nums = [0] * 100
@@ -10,3 +11,19 @@ for i in range(100):
     nums[i] = i * i
 
 print("Winner: {}".format(isWinner(100, nums)))
+
+
+# ./a.out barfoothefoobarman foo bar wut
+#                            wut foo bar
+
+# Basically look for where foo is at and add that starting index where I found foo at in a list
+# build the list with the subsequent words and their indexes
+
+# sort the array [7, 10, 13]
+
+# if the array == number of words we had to look for
+#     and
+# we have a consistent gap between all of the words
+#         i.e foo started at index 7 and bar at 10 and wut and 13
+#         we will look for 7 and then 10 and then 13 in the array
+#         if we couldn't find the consistnt gap, err, we failed
--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -11,37 +11,28 @@ def set_generator(number):
     }
 
 
-def play_game(playset, playset_max_num):
+def play_game(playset):
     """Plays a game eliminating all of the prime numbers"""
-    # Find the smallest prime number and assumes the initial user chooses the prime number
+
+    def is_prime(number):
+        """Returns whether a given number is a prime"""
+        if number in cache:
+            return cache[number]
+
+        for num in range(2, number):
+            if number % num == 0:
+                cache[number] = False
+                return cache[number]
+
+        cache[number] = True
+        return cache[number]
+
     playcount = 0
     for num in playset:
         if num != 1 and is_prime(num):
             playcount += 1
 
-    return (playcount % 2 == 0)
-
-
-def is_prime(number):
-    """Returns whether a given number is a prime"""
-    if number in cache:
-        return cache[number]
-
-    for num in range(2, number):
-        if number % num == 0:
-            cache[number] = False
-            return cache[number]
-
-    cache[number] = True
-    return cache[number]
-
-
-def remove_multiples(prime_number, number_set, number_set_max_num):
-    """Removes all of the multiples of num from number_set"""
-    i = 1
-    while prime_number * i <= number_set_max_num:
-        number_set.remove(prime_number * i)
-        i += 1
+    return playcount % 2 == 0
 
 
 def isWinner(x, nums):
@@ -52,15 +43,14 @@ def isWinner(x, nums):
         'False': 0
     }
 
-    if (x <= 0 or len(nums) <= 0):
+    if not x or not len(nums):
         return None
 
     for num in nums:
         playset = set_generator(num)
-        win = play_game(playset, num)
-        win_count[str(win)] += 1
-
-    # print(win_count)
+        win_count[
+            str(play_game(playset, num))
+        ] += 1
 
     return (
         'Ben'
