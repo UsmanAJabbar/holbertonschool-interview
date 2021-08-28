e08d866e6be13d0a623ab1bba781450a11458980
UsmanGTA <hi@usmanjabbar.com>
2021-08-28 17:44:46 +0500

    Added

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Prime Numbers"""
 
+cache = {}
+
 
 def set_generator(number):
     """Generates a set from {1...num}"""
@@ -9,30 +11,36 @@ def set_generator(number):
     }
 
 
-def play_game(playset):
+def play_game(playset, playset_max_num):
     """Plays a game eliminating all of the prime numbers"""
-
-    cache = {}
-
-    def is_prime(number):
-        """Returns whether a given number is a prime"""
-        if number in cache:
-            return cache[number]
-
-        i = 2
-        while i < (number // 2) + 1:
-            if number % i == 0:
-                break
-
-        cache[number] = number % i == 0
-        return cache[number]
-
     playcount = 0
     for num in playset:
         if num != 1 and is_prime(num):
             playcount += 1
 
-    return playcount % 2 == 0
+    return (playcount % 2 == 0)
+
+
+def is_prime(number):
+    """Returns whether a given number is a prime"""
+    if number in cache:
+        return cache[number]
+
+    for num in range(2, number):
+        if number % num == 0:
+            cache[number] = False
+            return cache[number]
+
+    cache[number] = True
+    return cache[number]
+
+
+def remove_multiples(prime_number, number_set, number_set_max_num):
+    """Removes all of the multiples of num from number_set"""
+    i = 1
+    while prime_number * i <= number_set_max_num:
+        number_set.remove(prime_number * i)
+        i += 1
 
 
 def isWinner(x, nums):
@@ -43,14 +51,15 @@ def isWinner(x, nums):
         'False': 0
     }
 
-    if x <= 0 or len(nums) <= 0:
+    if (x <= 0 or len(nums) <= 0):
         return None
 
     for num in nums:
         playset = set_generator(num)
-        win_count[
-            str(play_game(playset))
-        ] += 1
+        win = play_game(playset, num)
+        win_count[str(win)] += 1
+
+    # print(win_count)
 
     return (
         'Ben'
